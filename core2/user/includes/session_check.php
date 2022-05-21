c52fea05b2740bb9fc80a21fb957882802f49cbf
whoami-10 <andayabeejhay@gmail.com>
2022-05-21 12:21:03 +0800

    delete

--- core2/user/includes/session_check.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-if (!isset($_SESSION['test'])) {
-    if ($_SESSION['test'] != 'test_session_value') {
-        header('location: temporary_login.php');
-        exit();
-    }
-}
